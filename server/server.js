1656cd74070947a4471bb5df87ec7d5b57bfaf2a
carrymycross7 <threet.ldustin@gmail.com>
2023-01-26 23:37:23 -0600

    wip

--- server/server.js
+++ server/server.js
@@ -2,23 +2,17 @@ const express = require('express');
 const app = express();
 const body_parser = require('body-parser');
 const cors = require('cors');
-const connect = require('./config/dbConn');
+const connect = require('./db-config/dbConn');
 
 const {QueryTypes} = require('sequelize')
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.SERVER_PORT || 3000;
 
-
-let corsOptions = {
-    origin: "http://localhost:3000"
-};
-
-app.use(cors(corsOptions));
 
 const sequelize = connect();
 
-app.get('/', async (req,res) => {
-    res.send('HOME');
+app.get('/test', (req,res) => {
+    res.send('hi')
 });
 
 app.listen(PORT, () => {
